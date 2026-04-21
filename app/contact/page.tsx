5bfb1dd3e545cab9ba1603bd76dcafbd8f85a66c
Gaurang Ashava <gaurangashava@gmail.com>
2026-04-22 01:50:59 +0530

    naa

--- app/contact/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-import { useEffect } from 'react'
-
-export default function ContactRedirect() {
-  useEffect(() => {
-    window.location.replace('/#contact')
-  }, [])
-  return null
-}
